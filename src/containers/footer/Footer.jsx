a202e5e2c5a67cb63d8b427338e2e1c3c7bd6939
HackCoded <hackcoded@gmail.com>
2024-06-02 21:06:40 +0000

    Update Footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => (
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
-        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+        <p>Maint st. James Town, <br /> All Rights Reserved</p>
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Links</h4>
@@ -32,14 +32,14 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
+        <p>Main st. James Town Accra </p>
+        <p>233 540234334</p>
         <p>info@payme.net</p>
       </div>
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>@2023 GPT-3. All rights reserved.</p>
     </div>
   </div>
 );
